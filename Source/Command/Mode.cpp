ef546736ef5c55bfedafaea0f40d1936154bc305
jooh <johnn55@naver.com>
2024-03-13 17:05:40 +0900

    fix: how to get ip

--- Source/Command/Mode.cpp
+++ Source/Command/Mode.cpp
@@ -78,7 +78,7 @@ void	Mode::execute(Resource& resource, Message message) {
 		}
 	}
 	std::string replyMessage;
-	replyMessage += ":" + client->getNickname() + "!~" + client->getUsername() + "@127.0.0.1";
+	replyMessage += ":" + client->getNickname() + "!~" + client->getUsername() + "@" + client->getIp();
 	for (size_t i = 0; i < message.getParam().size(); i++) {
 		if (i == 2) replyMessage += ' ' + deleteFailedMode(modestring);
 		else if (notValidArgument.find(i) != notValidArgument.end()) continue;
