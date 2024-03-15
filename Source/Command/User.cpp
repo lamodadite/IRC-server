b24f4a7e22ddb26f88383b6678aa5f30e8de9211
Sunghun Yoon <gnsals6004@gmail.com>
2024-03-15 19:34:26 +0900

    fix: if pass is not present, ignore user, nick command

--- Source/Command/User.cpp
+++ Source/Command/User.cpp
@@ -8,7 +8,9 @@ User::~User() { }
 void User::execute(Resource& resource, Message message) {
 	Client*	client = resource.findClient(message.getClientFd());
 	
-	if (message.getParam().size() < 5) {
+	if (!client->getPassed()) {
+		return ;
+	} else if (message.getParam().size() < 5) {
 		reply.errNeedMoreParams(client, message.getFirstParam());
 		return ;
 	} else if (client->getRegistered()) {
